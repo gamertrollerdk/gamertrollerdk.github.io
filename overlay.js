bb6851bd5ab26fc41654ff63614bd6c9878cf85d
jsr <jonasschmidt0@hotmail.com>
2023-01-19 12:51:55 +0100

    close

---
+++ overlay.js
@@ -0,0 +1,27 @@
+                      //Cv
+                      window.addEventListener('DOMContentLoaded', function() {
+                        let openOverlayBtn = document.getElementById("openCV");
+                      let overlay = document.getElementById("Project");
+                      let closeOverlayBtn = document.getElementById("closeCV");
+                      openOverlayBtn.addEventListener("click", function() {
+                        overlay.style.display = "block";
+                      });
+                      closeOverlayBtn.addEventListener("click", function() {
+                        overlay.style.display = "none";
+                      });
+                    });
+                                          //test
+                    window.addEventListener('DOMContentLoaded', function() {
+                        let openOverlayBtn = document.getElementById("openTest");
+                      let overlay = document.getElementById("Project");
+                      let closeOverlayBtn = document.getElementById("closeTest");
+                    
+                    
+                      openOverlayBtn.addEventListener("click", function() {
+                        overlay.style.display = "block";
+                      });
+                      closeOverlayBtn.addEventListener("click", function() {
+                        overlay.style.display = "none";
+                      });
+                    });
+                    
